28a71d5519be2848f68bf1312ee14ee639be6bf3
Indrajit Raychaudhuri <irc@indrajit.com>
2010-05-15 22:31:30 +0530

    disable paredit stuff for now, seems broken

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -4,20 +4,21 @@
 
 (add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 (add-hook 'js-mode-hook 'moz-minor-mode)
-(add-hook 'js-mode-hook 'esk-paredit-nonlisp)
+; (add-hook 'js-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'js-mode-hook 'run-coding-hook)
 (setq js-indent-level 2)
 
-(eval-after-load 'js
-  '(progn (define-key js-mode-map "{" 'paredit-open-brace)
-          (define-key js-mode-map "}" 'paredit-close-brace-and-newline)
-          ;; fixes problem with pretty function font-lock
-          (define-key js-mode-map (kbd ",") 'self-insert-command)
-          (font-lock-add-keywords
-           'js-mode `(("\\(function *\\)("
-                             (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
-                                       nil)))))))
+; FIXME
+; (eval-after-load 'js
+;   '(progn (define-key js-mode-map "{" 'paredit-open-brace)
+;           (define-key js-mode-map "}" 'paredit-close-brace-and-newline)
+;           ;; fixes problem with pretty function font-lock
+;           (define-key js-mode-map (kbd ",") 'self-insert-command)
+;           (font-lock-add-keywords
+;            'js-mode `(("\\(function *\\)("
+;                              (0 (progn (compose-region (match-beginning 1)
+;                                                        (match-end 1) "ƒ")
+;                                        nil)))))))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
