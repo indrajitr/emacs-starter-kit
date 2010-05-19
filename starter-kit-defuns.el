8e9004816d547b711fa5c4bc88550ed294eb82ed
Indrajit Raychaudhuri <irc@indrajit.com>
2010-05-20 03:41:00 +0530

    regen-autoloads and recompile-init now works with multiple package directory and not just 'elpa-to-submit'

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -147,20 +147,23 @@ Symbols matching the text at point are put first in the completion list."
   (interactive)
   (byte-recompile-directory dotfiles-dir 0)
   ;; TODO: remove elpa-to-submit once everything's submitted.
-  (byte-recompile-directory (concat dotfiles-dir "elpa-to-submit/") 0))
+  ;; (byte-recompile-directory (concat dotfiles-dir "elpa-to-submit/") 0)
+  (mapc (lambda (d) (byte-recompile-directory d 0)) packages-dir))
 
 (defun regen-autoloads (&optional force-regen)
   "Regenerate the autoload definitions file if necessary and load it."
   (interactive "P")
-  (let ((autoload-dir (concat dotfiles-dir "/elpa-to-submit"))
-        (generated-autoload-file autoload-file))
-    (when (or force-regen
-              (not (file-exists-p autoload-file))
-              (some (lambda (f) (file-newer-than-file-p f autoload-file))
-                    (directory-files autoload-dir t "\\.el$")))
-      (message "Updating autoloads...")
-      (let (emacs-lisp-mode-hook)
-        (update-directory-autoloads autoload-dir))))
+  (mapc (lambda (d)
+          (let ((autoload-dir d)
+                (generated-autoload-file autoload-file))
+            (when (or force-regen
+                      (not (file-exists-p autoload-file))
+                      (some (lambda (f) (file-newer-than-file-p f autoload-file))
+                            (directory-files autoload-dir t "\\.el$")))
+              (message "Updating autoloads...")
+              (let (emacs-lisp-mode-hook)
+                (update-directory-autoloads autoload-dir)))))
+        packages-dir)
   (load autoload-file))
 
 (defun sudo-edit (&optional arg)
